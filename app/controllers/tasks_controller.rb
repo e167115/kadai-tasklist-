447e0e5e99c886fd3cc24267cdc2e303ba165a01
yuta <e167115ty@gmail.com>
2020-03-08 09:44:15 +0000

    add bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:edit, :show, :update, :destroy]
     def index
-        @tasks = Task.all
+        @tasks = Task.all.page(params[:page]).per(5)
     end
 
     def create
