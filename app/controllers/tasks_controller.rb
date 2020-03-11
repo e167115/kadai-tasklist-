b10747231191b4389b8c004994fa6d4838d0a20f
yuta <e167115ty@gmail.com>
2020-03-11 07:50:42 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,16 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:edit, :show, :update, :destroy]
-    before_action :require_user_logged_in, only: [:index, :show]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     def index
-        @tasks = Task.all.page(params[:page]).per(5)
+        if logged_in?
+            @task = current_user.tasks.build
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
 
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         if @task.save 
             flash[:success] = "追加完了"
             redirect_to @task
@@ -49,10 +53,20 @@ class TasksController < ApplicationController
     
     def set_task
         @task = Task.find(params[:id])
+        
     end
-    
+        
     def task_params
         params.require(:task).permit(:content, :status)
     end
+
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to tasks_path
+      end
+    end        
+
+
 
 end
